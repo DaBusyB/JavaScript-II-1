2f32b905340bd4c684df4363540483675f113f86
Brandi Apetsi <Brandi@Brandis-iMac.local>
2019-01-21 14:42:54 -0500

    work on array-methods.js

--- README.md
+++ README.md
@@ -5,10 +5,10 @@ With some basic JavaScript principles in hand, we can now expand our skills out 
 
 ## Task 1: Set Up The Project With Git
 
-* [ ] Fork the project into your GitHub user account
-* [ ] Clone the forked project into a directory on your machine
-* [ ] You are now ready to build this project with your preferred IDE
-* [ ] To test your `console.log()` statements, open up the index.html file found in the assignments folder and use the developer tools to view the console.  
+* [x ] Fork the project into your GitHub user account
+* [ x] Clone the forked project into a directory on your machine
+* [ x] You are now ready to build this project with your preferred IDE
+* [ x] To test your `console.log()` statements, open up the index.html file found in the assignments folder and use the developer tools to view the console.  
 
 
 ## Task 2: Callbacks
@@ -25,11 +25,11 @@ This task focuses on getting practice with callback functions by giving you an a
 
 Use `.forEach()`, `.map()`, `.filter()`, and `.reduce()` to loop over an array with 50 objects in it. The [array-methods.js](assignments/array-methods.js) file contains several challenges built around a fundraising 5K fun run event.
 
-* [ ] Review the contents of the [array-methods.js](assignments/array-methods.js) file.  
+* [ x] Review the contents of the [array-methods.js](assignments/array-methods.js) file.  
 
-* [ ] Complete the problems provided to you
+* [ x] Complete the problems provided to you
 
-* [ ] Notice the last three problems are up to you to create and solve.  This is an awesome opportunity for you to push your critical thinking about array methods, have fun with it.
+* [ x] Notice the last three problems are up to you to create and solve.  This is an awesome opportunity for you to push your critical thinking about array methods, have fun with it.
 
 ## Task 4: Closures
 
--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -1,9 +1,10 @@
-// A local community center is holding a fund rasising 5k fun run and has invited 50 small businesses to make a small donation on their behalf for some much needed updates to their facilities.  Each business has assigned a representative to attend the event along with a small donation.
+// A local community center is holding a fund raising 5k fun run and has invited 50 small businesses to make a small donation on their behalf for some much needed updates to their facilities.  Each business has assigned a representative to attend the event along with a small donation.
 
 // Scroll to the bottom of the list to use some advanced array methods to help the event director gather some information from the businesses.
 
 
-const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"cseiler0@wired.com","shirt_size":"2XL","company_name":"Divanoodle","donation":75},
+const runners = [
+   {"id":1,"first_name":"Charmain","last_name":"Seiler","email":"cseiler0@wired.com","shirt_size":"2XL","company_name":"Divanoodle","donation":75},
 {"id":2,"first_name":"Whitaker","last_name":"Ierland","email":"wierland1@angelfire.com","shirt_size":"2XL","company_name":"Wordtune","donation":148},
 {"id":3,"first_name":"Julieta","last_name":"McCloid","email":"jmccloid2@yahoo.com","shirt_size":"S","company_name":"Riffpedia","donation":171},
 {"id":4,"first_name":"Martynne","last_name":"Paye","email":"mpaye3@sciencedaily.com","shirt_size":"XL","company_name":"Wordware","donation":288},
@@ -57,28 +58,63 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
-console.log(fullName);
+
+runners.forEach(function(eaRunner) {
+    fullName.push(`${eaRunner.first_name} ${eaRunner.last_name}`);
+})
+
+console.log("challenge 1", fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
-console.log(allCaps); 
+
+runners.map(function(eaRunner) {
+    allCaps.push(`${eaRunner.first_name}`.toUpperCase());
+})
+
+console.log("challenge 2", allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
-console.log(largeShirts);
+
+const lRunner = runners.filter( runner => runner.shirt_size === "L");
+largeShirts.push(lRunner);
+
+console.log("challenge 3", largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
-console.log(ticketPriceTotal);
+
+const total = runners.reduce(function(accumulator, currentValue) {
+    return accumulator + currentValue.donation;
+}, 0);
+
+ticketPriceTotal.push(total);
+
+console.log("challenge 4", ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+//sort runners by donation amount
+let sortedDonations = [];
+
+const donationSort  = runners.sort((a, b) => a.donation - b.donation)
+sortedDonations.push(donationSort);
+
+console.log("challenge 5.1", sortedDonations)
 
 // Problem 2
+//reverse the array of runners
+
+const reverseRunners = runners.reverse();
+console.log("challenge 5.2", reverseRunners)
 
 // Problem 3
+// 
+
+console.log("challenge 5.3")
